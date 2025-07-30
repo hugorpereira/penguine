d9bbdfd5cd3e3b5287816cb8a02a0ee7f2b572c9
hugorpereira <hugorp.mail@gmail.com>
2025-07-30 14:54:17 -0600

    fix: update app

--- app.py
+++ app.py
@@ -1,85 +1,77 @@
 import pandas as pd
 import streamlit as st
 import numpy as np
-import io
 from sklearn.ensemble import RandomForestClassifier
 
-st.title("Penguine species prediction")
-st.info("This is end-to-end Machine Learning app")
+st.title("Penguine Speicy Prediction ML app")
+st.info("This is end-to-end Machine Learning App")
 
 with st.expander("Data"):
-    st.write("**Raw data**")
-    df = pd.read_csv("data/penguins_cleaned.csv")
-    df
+  st.write("**Raw data**")
+  df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+  df
 
-    st.write("Input variables")
-    X_raw = df.drop("species", axis=1)
-    X_raw
+  st.write("Input variables")
+  X_raw = df.drop('species',axis=1)
+  X_raw
 
-    st.write("Target variable")
-    y_raw = df.species
-    y_raw
-    
-    st.write("Descriptive Statistics")
-    desc = df.describe()
-    desc
+  st.write("Target variable")
+  y_raw = df.species
+  y_raw
 
-    st.write("More information about Data")
-    buffer = io.StringIO()
-    df.info(buf=buffer)
-    info_string = buffer.getvalue()
-    st.text(info_string)
+  st.write("Descriptive Statistics")
+  des = df.describe()
+  des
+
+  st.write("More information about Data")
+  inf = df.info()
+  st.text(inf)
+  
 
 with st.expander("Data Visualization"):
-    st.scatter_chart(data=df, x='bill_length_mm',y='body_mass_g',color='species')
+  st.scatter_chart(data=df, x='bill_length_mm',y='body_mass_g',color='species')
 
-with st.expander("Input Data"):
-    pass
 
-with st.expander("Data preparation"):
-    pass
+
+
+  
 
 with st.sidebar:
-    st.header("Input Variables")
-    island = st.selectbox("Island", ("Biscoe","Dream", "Torgersen"))
-    bill_length_mm = st.slider("Bill length (mm)", 32.1, 59.6, 43.9)
-    bill_depth_mm = st.slider("Bill depth (mm)", 13.1, 21.5, 17.2)
-    flipper_depth_mm = st.slider("Flipper depth (mm)", 172.0, 231.0, 201.0)
-    body_mass_g = st.slider("Body mass (g)", 2700.0, 6300.0, 4207.0)
-    sex = st.selectbox("Gender", ("male", "female"))
+  st.header("Input Variables")
+  island = st.selectbox('Island',('Biscoe','Dream','Torgersen'))
+  bill_length_mm = st.slider('Bill length (mm)',32.1,59.6,43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)',13.1,21.5,17.2)
+  flipper_length_mm = st.slider('Flipper length (mm)',172.0,231.0,201.0)
+  body_mass_g = st.slider('Body mass (g)',2700.0,6300.0,4207.0)
+  gender = st.selectbox('Gender',('male','female'))
 
-    data = {
-        "island": island,
-        "bill_length_mm": bill_length_mm,
-        "bill_length_mm": bill_length_mm,
-        "flipper_depth_mm": flipper_depth_mm,
-        "body_mass_g": body_mass_g,
-        "sex": sex
-    }
+  data = {'island':island,
+          'bill_length_mm':bill_length_mm,
+          'bill_depth_mm': bill_depth_mm,
+          'flipper_length_mm': flipper_length_mm,
+          'body_mass_g': body_mass_g,
+          'gender': gender  
+  }
+  input_df = pd.DataFrame(data, index=[0])
+  input_penguins = pd.concat([input_df, X_raw], axis=0)
 
-    input_df = pd.DataFrame(data,index=[0])
-    input_penguins = pd.concat([input_df, X_raw], axis=0)
-    input_penguins
+with st.expander("Input data"):
+  st.write("**Input data**")
+  input_df
+  st.write("**Combined data**")
+  input_penguins
+# One hot encoding for X
+encode = ['island','sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+X = df_penguins[1:]
+input_row = df_penguins[:1]
+# One hot encoding for y
+target_mapper = {
+  'Adelie': 0,
+  'Chinstrap': 1,
+  'Gentoo': 2
+}
+def target_encode(val):
+  return target_mapper[val]
 
-    with st.expander("Input data"):
-        st.write("**Input data**")
-        input_df
-
-        st.write("**Combined data**")
-        input_penguins
-    
-    encode = ["island", "sex"]
-    df_penguins = pd.get_dummies(input_penguins, prefix = encode)
-
-    X = df_penguins[1:]
-    input_row = df_penguins[:1]
-
-    target_mapper = {
-        'Adelie': 0,
-        'Chinstrap': 1,
-        'Gentoo': 2
-    }
-    def target_encode(val):
-        return target_mapper[val]
-    
-    y = y_raw.apply(target_encode)
+y = y_raw.apply(target_encode)
