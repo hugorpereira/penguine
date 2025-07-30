e1b1f1e1b7dbe5a0f927599abc3bc7854d6384d4
hugorpereira <hugorp.mail@gmail.com>
2025-07-30 08:29:30 -0600

    fix import RandomForestClassifier

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit
 import numpy as np
-from sklearn.ensemble important RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 st.title("Penguine species prediction ML app")
