fb33824160c408d118dd0b7196117ba9e551a366
hugorpereira <hugorp.mail@gmail.com>
2025-07-30 14:59:30 -0600

    fix: update data columns

--- app.py
+++ app.py
@@ -50,8 +50,8 @@ with st.sidebar:
     data = {
         "island": island,
         "bill_length_mm": bill_length_mm,
-        "bill_length_mm": bill_length_mm,
-        "flipper_depth_mm": flipper_depth_mm,
+        "bill_depth_mm": bill_depth_mm,
+        "flipper_length_mm": flipper_length_mm,
         "body_mass_g": body_mass_g,
         "sex": gender
     }
