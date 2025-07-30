5beeecd0ffe6d50ef336b8eaf7f1b6dd6da0b5cf
hugorpereira <hugorp.mail@gmail.com>
2025-07-30 09:15:58 -0600

    fix: show dt.info

--- app.py
+++ app.py
@@ -24,7 +24,10 @@ with st.expander("Data"):
     desc
 
     st.write("More information about Data")
-    df.info()
+    buffer = io.StringIO()
+    df.info(buf=buffer)
+    info_string = buffer.getvalue()
+    st.text(info_string)
 
 with st.expander("Data Visualization"):
     pass
